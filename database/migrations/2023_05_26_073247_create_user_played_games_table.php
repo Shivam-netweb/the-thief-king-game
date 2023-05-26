e1b21dd338371fa3eb1857894255a5d0c6a4eea3
shivam netweb <102536014+Shivam-netweb@users.noreply.github.com>
2023-05-26 13:10:41 +0530

    init commit

---
+++ database/migrations/2023_05_26_073247_create_user_played_games_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_played_games', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('game_id');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('game_id')->on('games')->references('id');
+            $table->foreign('user_id')->on('users')->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_played_games');
+    }
+};
+
---
+++ database/migrations/2023_05_26_073506_create_rounds_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rounds', function (Blueprint $table) {
+            $table->id();
+            $table->integer('round_count');
+            $table->unsignedBigInteger('game_id');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('game_id')->on('games')->references('id');
+            $table->foreign('user_id')->on('users')->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rounds');
+    }
+};
